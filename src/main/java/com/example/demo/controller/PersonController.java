7670f1765090e07659f19aa91f89fe074ce68218
jesselmk@163.com <jesselmk@163.com>
2019-08-10 12:48:32 +0800

    remove file

--- src/main/java/com/example/demo/controller/PersonController.java
+++ src/main/java/com/example/demo/controller/PersonController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class PersonInfoController {
+public class PersonController {
     @Autowired
     private PersonInfoService personInfoService;
 
